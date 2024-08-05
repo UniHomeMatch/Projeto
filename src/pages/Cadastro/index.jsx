e3e0a02245a391552b92d48ecb1e7f70da1ce2e6
Danilo Barbieri <danilobarbieri030@gmail.com>
2024-08-05 19:31:44 -0300

    feat: Modificação do formulario de registro

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,5 +1,5 @@
 import React from'react';
-import { Container, ContainerForm, Form, Label, Links, Mask } from './styles';
+import { Container, ContainerForm, Form, Label, Links, Mask, Select } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
@@ -14,11 +14,20 @@ const Cadastro = () => {
       <ContainerForm>
         <Form>
             <Label for="username">Usuário</Label>
-            <Input type="text" name="username" id="username" placeholder="Usuário" autocomplete="off" required />
+            <Input type="text" name="username" id="name" placeholder="Usuário" autocomplete="off" required />
+            <Label for="CPF">CPF</Label>
+            <Mask mask="999.999.999-99" name="CPF" id="cpf" placeholder="Digite seu CPF" autocomplete="off" required />
+            <Label for="birthdate">Data de Nascimento</Label>
+            <Input type="date" name="birthdate" id="birthdate" placeholder="Digite sua Data de Nascimento" required />
+            <Label for="genero"> Gênero </Label>
+            <Select name="genero" id="genero" placeholder="Selecione seu Gênero" required>
+              <option className='selecione' value="" disabled selected>Selecione seu gênero</option>
+              <option value="1">Masculino</option>
+              <option value="2">Feminino</option>
+              <option value="3">Outros</option>
+            </Select>
             <Label for="email">Email</Label>
             <Input type="text" name="email" id="email" placeholder="Digite seu Email" autocomplete="off" required />
-            <Label for="CPF">CPF</Label>
-            <Mask mask="999.999.999-99" name="CPF" id="CPF" placeholder="Digite seu CPF" autocomplete="off" required />
             <Label for="password">Senha</Label>
             <Input type="password" name="password" id="password" placeholder="Digite sua Senha" autocomplete="off" required/>
             <Links>
--- src/pages/Cadastro/styles.js
+++ src/pages/Cadastro/styles.js
@@ -54,3 +54,18 @@ export const Mask = styled(ReactInputMask)`
     border: 1px solid var(--gray);
     resize: none;
 `
+
+export const Select = styled.select`
+    height: 50px;
+    padding: 8px 15px;
+    border-radius: 5px;
+    width: 100%;
+    color: var(--secondary);
+    letter-spacing: 1px;
+    font-size: 15px;
+    margin-bottom: 10px;
+    outline: none;
+    border-color: var(--gray);
+    border: 1px solid var(--gray);
+    resize: none;
+    `
