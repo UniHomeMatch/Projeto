d0ea5fb39307db650241b632fdaa8bdb13126cb8
Danilo Barbieri <danilobarbieri030@gmail.com>
2024-08-20 15:06:22 -0300

    fix: Modificado api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+export const urlApi = 'http://localhost:8000';
+
 const api = axios.create({
     baseURL: `http://localhost:8000`,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+    //headers: {
+    //    'Content-Type': 'application/json'
+    //}
 })
 
 export default api;
