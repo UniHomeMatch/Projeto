ba4a1857ec453df45807758ee7984a2d4ff37778
Danilo Barbieri <danilobarbieri030@gmail.com>
2024-10-24 21:43:58 -0300

    Troca de Api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const urlApi = 'http://localhost:8000';
+export const urlApi = 'http://64.181.165.60:9090';
 
 const api = axios.create({
-    baseURL: `http://localhost:8000`,
+    baseURL: `http://64.181.165.60:9090`,
     //headers: {
     //    'Content-Type': 'application/json'
     //}
