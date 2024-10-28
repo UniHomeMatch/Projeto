4eb0a046957cd8e46ced19b040323567d1103117
giovana <132526932+GiovanaGiorgi@users.noreply.github.com>
2024-10-28 19:44:37 -0300

    http

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const urlApi = 'https://144.22.141.202:9090';
+export const urlApi = 'http://144.22.141.202:9090';
 
 const api = axios.create({
-    baseURL: `https://144.22.141.202:9090`,
+    baseURL: `http://144.22.141.202:9090`,
     //headers: {
     //    'Content-Type': 'application/json'
     //}
