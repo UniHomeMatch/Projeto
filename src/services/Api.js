1aedac822f6a298394b0b99a5894b7fccf763aca
DaniloBarbieri <79670102+danilo-barbieri@users.noreply.github.com>
2024-10-31 16:21:36 -0300

    Update Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,12 +1,10 @@
+
 import axios from "axios";
 
-export const urlApi = '/api';
+export const urlApi = '/api'; 
 
 const api = axios.create({
-    baseURL: `http://144.22.141.202:9090`,
-    //headers: {
-    //    'Content-Type': 'application/json'
-    //}
-})
+    baseURL: urlApi,
+});
 
 export default api;
